e46ec3c3cf02eefb7a562a1f5792084c2dbe0309
Will Barnes <wtb2@rice.edu>
2016-08-30 18:16:59 -0500

    adding class for constructing loop objects for every fieldline in the extrapolated field

---
+++ synthesizAR/loop.py
@@ -0,0 +1,45 @@
+"""
+Class for an individual loop structure that is part of a larger active region.
+"""
+
+import numpy as np
+import astropy.units as u
+
+class Loop(object):
+    """
+    Coronal loop object for easily handling all of the properties associated with a loop in an active region.
+
+    Parameters
+    ----------
+    Attributes
+    ----------
+    Notes
+    -----
+    """
+
+    def __init__(self,name,coordinates,field_strength):
+        """
+        Constructor
+        """
+        #set unique label for loop object
+        self.name = name
+        #Load in cartesian coordinates, assign units as centimeters
+        self.coordinates = coordinates*u.cm
+        #Load in field strength along the field line; convert from Tesla to Gauss
+        self.field_strength = (np.array(field_strength)*u.T).to(u.Gauss)
+
+
+    @property
+    def field_aligned_coordinate(self):
+        """
+        Field-aligned coordinate s. This will have the same units the original coordinates.
+        """
+        return np.append(0., np.linalg.norm(np.diff(self.coordinates.value,axis=0), axis=1).cumsum())*self.coordinates.unit
+
+
+    @property
+    def full_length(self):
+        """
+        Loop full-length 2L. This will have the same units as the original coordinates.
+        """
+        return np.sum(np.linalg.norm(np.diff(self.coordinates.value,axis=0),axis=1))*self.coordinates.unit
