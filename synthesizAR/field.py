some style fixes and units checking on loop length range

synthesizAR/field.py
@@ -47,7 +47,6 @@ class Skeleton(object):
         else:
             self.logger.warning('No HMI fits file supplied. A new HMI map object will not be created.')
 
-
     def _process_map(self,tmp_map,crop=None,resample=None):
         """
         Rotate, crop and resample map if needed. Can do any other needed processing here too.
@@ -61,7 +60,6 @@ class Skeleton(object):
         resample : `~astropy.units.Quantity`, `[new_xdim,new_ydim]`, optional
             The new x- and y-dimensions of the resampled map, should have the same units as `map.dimensions.x` and `map.dimensions.y`
         """
-
         tmp_map = tmp_map.rotate()
         if crop is not None:
             tmp_map = tmp_map.submap(crop[0],crop[1])
@@ -70,12 +68,10 @@ class Skeleton(object):
 
         self.hmi_map = tmp_map
 
-
     def save_field(self,savedir=None):
         """
         Save the components of the field object to be reloaded later.
         """
-
         if savedir is None:
             savedir = 'synthesizAR-field-save_' + datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
         if not os.path.exists(savedir):
@@ -95,7 +91,6 @@ class Skeleton(object):
         #3d extrapolated field
         with open(os.path.join(savedir,'map_3d.pickle'),'wb') as f:
             pickle.dump(self._map_3d,f)
-
 
     @classmethod
     def restore_field(cls,savedir):
@@ -128,15 +123,12 @@ class Skeleton(object):
 
         return field
 
-
     def _convert_angle_to_length(self,angle_or_length,working_units=u.meter):
         """
         Recast the `synthesizAR.util.convert_angle_to_length` to automatically use the supplied HMI map.
         """
-
-        result = convert_angle_to_length(self.hmi_map,angle_or_length,working_units=working_units)
-        return result
-
+        return convert_angle_to_length(self.hmi_map,angle_or_length,
+                                        working_units=working_units)
 
     def _transform_to_yt(self,map_3d,boundary_clipping=(2,2,2)):
         """
@@ -149,7 +141,6 @@ class Skeleton(object):
         boundary_clipping : `tuple`, optional
             The extrapolated volume has a layer of ghost cells in each dimension. This tuple of (nx,ny,nz) tells how many cells we need to contract the volume and map in each direction.
         """
-
         #reshape the magnetic field data
         _tmp = map_3d.data[boundary_clipping[0]:-boundary_clipping[0], boundary_clipping[1]:-boundary_clipping[1], boundary_clipping[2]:-boundary_clipping[2],:]
         #some annoying and cryptic translation between yt and SunPy
@@ -157,25 +148,22 @@ class Skeleton(object):
                     Bx=(np.swapaxes(_tmp[:,:,:,1],0,1),'T'),
                     By=(np.swapaxes(_tmp[:,:,:,0],0,1),'T'),
                     Bz=(np.swapaxes(_tmp[:,:,:,2],0,1),'T'))
-
         #trim the boundary hmi map appropriately
         self.clipped_hmi_map = self.hmi_map.submap(
                                 self.hmi_map.xrange+self.hmi_map.scale.x*u.Quantity([boundary_clipping[0]*u.pixel,-boundary_clipping[0]*u.pixel]),
                                 self.hmi_map.yrange+self.hmi_map.scale.y*u.Quantity([boundary_clipping[1]*u.pixel,-boundary_clipping[1]*u.pixel]))
-
         #create the bounding box
         bbox = np.array([
             self._convert_angle_to_length(self.clipped_hmi_map.xrange).value,
             self._convert_angle_to_length(self.clipped_hmi_map.yrange).value,
             self._convert_angle_to_length(map_3d.zrange+map_3d.scale.z*u.Quantity([boundary_clipping[2]*u.pixel,-boundary_clipping[2]*u.pixel])).value])
-
         #assemble the dataset
         self.extrapolated_3d_field = yt.load_uniform_grid(data,
             data['Bx'][0].shape, bbox=bbox, length_unit=yt.units.cm,
             geometry=('cartesian',('x','y','z')))
 
-
-    def _filter_streamlines(self,streamline,close_threshold=0.05,loop_length_range=[2.e+9,5.e+10]):
+    @u.quantity_input(loop_length_range=u.cm)
+    def _filter_streamlines(self,streamline,close_threshold=0.05,loop_length_range=[2.e+9,5.e+10]*u.cm):
         """
         Check extracted loop to make sure it fits given criteria. Return
         True if it passes.
@@ -185,26 +173,22 @@ class Skeleton(object):
         streamline : yt streamline object
         close_threshold : `float`
             percentage of domain width allowed between loop endpoints
-        loop_length_range : `tuple`
+        loop_length_range : `~astropy.Quantity`
             minimum and maximum allowed loop lengths (in centimeters)
         """
-
         streamline = streamline[np.all(streamline != 0.0, axis=1)]
         loop_length = np.sum(np.linalg.norm(np.diff(streamline,axis=0),axis=1))
-
         if np.fabs(streamline[0,2] - streamline[-1,2]) > close_threshold*self.extrapolated_3d_field.domain_width[2]:
             return False
-        elif loop_length > loop_length_range[1] or loop_length < loop_length_range[0]:
+        elif loop_length > loop_length_range[1].to(u.cm).value or loop_length < loop_length_range[0].to(u.cm).value:
             return False
         else:
             return True
-
 
     def extrapolate_field(self,zshape,zrange,use_numba_for_extrapolation=True):
         """
         Extrapolate the 3D field and transform it into a yt data object.
         """
-
         #extrapolate field
         self.logger.debug('Extrapolating field.')
         extrapolator = solarbextrapolation.extrapolators.PotentialExtrapolator(self.hmi_map, zshape=zshape, zrange=zrange)
@@ -215,13 +199,12 @@ class Skeleton(object):
         self.logger.debug('Transforming to yt data object')
         self._transform_to_yt(map_3d)
 
-
     def extract_streamlines(self,number_fieldlines,max_tries=100):
         """
         Trace the fieldlines through extrapolated 3D volume
         """
         #trace field and return list of field lines
-        self.logger.debug('Tracing fieldlines')
+        self.logger.info('Tracing fieldlines')
         self.streamlines = []
         seed_points = []
         i_tries = 0
@@ -248,12 +231,10 @@ class Skeleton(object):
         if i_tries == max_tries:
             self.logger.warning('Maxed out number of tries. Only found {} acceptable streamlines'.format(len(self.streamlines)))
 
-
     def peek(self, figsize=(10,10), color=sns.color_palette('deep')[0], alpha=0.75, print_to_file=None, **kwargs):
         """
         Show extracted fieldlines overlaid on HMI image.
         """
-
         fig = plt.figure(figsize=figsize)
         ax = fig.gca(projection=self.hmi_map)
         self.hmi_map.plot()
@@ -269,7 +250,6 @@ class Skeleton(object):
             plt.savefig(print_to_file,**kwargs)
         plt.show()
 
-
     def make_loops(self):
         """
         Make list of loop objects from the extracted streamlines
@@ -279,14 +259,12 @@ class Skeleton(object):
             loops.append(Loop('loop{}'.format(i), stream[0].value, stream[1].value))
         self.loops = loops
 
-
     def configure_loop_simulations(self, interface, parent_config_dir='config', parent_results_dir='results'):
         """
         Configure hydrodynamic simulations for each loop object
         """
         for loop in self.loops:
             interface.configure_input(loop,parent_config_dir,parent_results_dir)
-
 
     def load_loop_simulations(self,interface,savefile=None,**kwargs):
         """
@@ -309,13 +287,12 @@ class Skeleton(object):
                 loop._temperature = temperature
                 loop._density = density
 
-
     def calculate_emissivity(self,emissivity_model,savefile=None,**kwargs):
         """
         Calculate emissivity as function of time and space for each loop
         """
         for loop in self.loops:
-            self.logger.info('Calculating emissivity for loop {}'.format(loop.name))
+            self.logger.debug('Calculating emissivity for loop {}'.format(loop.name))
             loop.wavelengths = emissivity_model.wavelengths
             emiss = emissivity_model.calculate_emissivity(loop.temperature,
                                                           loop.density,