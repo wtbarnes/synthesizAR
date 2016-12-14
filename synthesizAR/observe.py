7488c6562c7a7d7f20c4f9bb5c37dee256db7d5e
Will Barnes <wtb2@rice.edu>
2016-12-14 17:03:33 -0600

    make timeinterpolation cubic spline

--- synthesizAR/observe.py
+++ synthesizAR/observe.py
@@ -93,7 +93,7 @@ class Observer(object):
                         #interpolate in s and t
                         f_s = interp1d(loop.field_aligned_coordinate.value,counts.value,axis=1)
                         interpolated_counts = interp1d(loop.time.value,f_s(interp_s),
-                                                        axis=0)(instr.observing_time)
+                                                        axis=0,kind='cubic')(instr.observing_time)
                         dset[:,start_index:(start_index+len(interp_s))] = interpolated_counts
                         if 'units' not in dset.attrs:
                             dset.attrs['units'] = counts.unit.to_string()
