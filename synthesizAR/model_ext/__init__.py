bf8edbb8168cf766dc1676dba51168362403a8c4
Will Barnes <wtb2@rice.edu>
2017-01-16 16:26:32 -0600

    make heating model helpers available

--- synthesizAR/model_ext/__init__.py
+++ synthesizAR/model_ext/__init__.py
@@ -4,3 +4,4 @@ Extensions for configuring input files for and loading results from hydrodynamic
 
 from .ebtel import EbtelInterface
 from .heating_models import UniformHeating,PowerLawScaledWaitingTimes,PowerLawUnscaledWaitingTimes
+from .heating_models import calculate_free_energy,power_law_transform
