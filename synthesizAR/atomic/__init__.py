52dc47e610c2c88c7bb7c9b3833d2e20f49f8fa3
Will Barnes <wtb2@rice.edu>
2017-02-15 13:38:31 -0600

    make the nei solver and data grabber importable

--- synthesizAR/atomic/__init__.py
+++ synthesizAR/atomic/__init__.py
@@ -4,3 +4,4 @@ Emissivity models and atomic calculations for simulating radiation from coronal 
 
 from .chianti import ChIon
 from .emission_models import EquilibriumEmissionModel
+from .nei import get_ion_info,solve_nei_populations
