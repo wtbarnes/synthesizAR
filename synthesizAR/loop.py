8f5ae6534e1f5eb61d44f76162f6d8550aef1397
Will Barnes <wtb2@rice.edu>
2017-02-25 17:42:09 -0600

    update loop for new emission requirements

--- synthesizAR/loop.py
+++ synthesizAR/loop.py
@@ -116,19 +116,18 @@ Maximum field strength : {max_b:.2f}
         else:
             return self._velocity_xyz
 
-    @u.quantity_input(wavelength=u.angstrom)
-    def get_emission(self,wavelength,return_ion_name=False):
+    def get_emission(self,key,return_ion_name=False):
         """
         Get the calculated emission (energy per unit volume per unit time per unit solid angle) for
         a particular wavelength. Can be stored in memory or pulled from an HDF5 file.
         """
         if hasattr(self,'emission_savefile'):
             with h5py.File(self.emission_savefile,'r') as hf:
-                dset = hf[os.path.join(self.name,str(wavelength.to(u.angstrom).value))]
+                dset = hf[os.path.join(self.name,key)]
                 ion_name = dset.attrs['ion_name']
                 emiss = np.array(dset)*u.Unit(dset.attrs['units'])
         else:
-            emiss = self._emission[wavelength]
+            emiss = self._emission[key]
 
         if return_ion_name:
             return emiss,ion_name
