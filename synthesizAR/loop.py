838ee7fffce0c13686bfded5129c079d883c0958
Will Barnes <wtb2@rice.edu>
2017-02-25 22:34:53 -0600

    sanitize keys for emission from loops

--- synthesizAR/loop.py
+++ synthesizAR/loop.py
@@ -123,11 +123,11 @@ Maximum field strength : {max_b:.2f}
         """
         if hasattr(self,'emission_savefile'):
             with h5py.File(self.emission_savefile,'r') as hf:
-                dset = hf[os.path.join(self.name,key)]
+                dset = hf[os.path.join(self.name,'{}'.format(key))]
                 ion_name = dset.attrs['ion_name']
                 emiss = np.array(dset)*u.Unit(dset.attrs['units'])
         else:
-            emiss = self._emission[key]
+            emiss = self._emission['{}'.format(key)]
 
         if return_ion_name:
             return emiss,ion_name
