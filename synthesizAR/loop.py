c84edfa459ffb24bf232466c882904dd70d72636
Will Barnes <wtb2@rice.edu>
2017-02-27 18:01:35 -0600

    revert back to sanitizing input keys when retrieving emission for loop

--- synthesizAR/loop.py
+++ synthesizAR/loop.py
@@ -116,18 +116,19 @@ Maximum field strength : {max_b:.2f}
         else:
             return self._velocity_xyz
 
-    def get_emission(self,key,return_ion_name=False):
+    @u.quantity_input(wavelength=u.angstrom)
+    def get_emission(self,wavelength,return_ion_name=False):
         """
         Get the calculated emission (energy per unit volume per unit time per unit solid angle) for
         a particular wavelength. Can be stored in memory or pulled from an HDF5 file.
         """
         if hasattr(self,'emission_savefile'):
             with h5py.File(self.emission_savefile,'r') as hf:
-                dset = hf[os.path.join(self.name,'{}'.format(key))]
+                dset = hf[os.path.join(self.name,'{}'.format(wavelength.to(u.angstrom).value))]
                 ion_name = dset.attrs['ion_name']
                 emiss = np.array(dset)*u.Unit(dset.attrs['units'])
         else:
-            emiss = self._emission['{}'.format(key)]
+            emiss = self._emission['{}'.format(wavelength.to(u.angstrom).value)]
 
         if return_ion_name:
             return emiss,ion_name
