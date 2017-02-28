5fbd3918456280c116b56ea9a7c33e9a50ee7b12
Will Barnes <wtb2@rice.edu>
2017-02-28 17:55:06 -0600

    fix get_emission call to use unitful wavelength

--- synthesizAR/instruments/hinode.py
+++ synthesizAR/instruments/hinode.py
@@ -99,7 +99,7 @@ class InstrumentHinodeEIS(InstrumentBase):
         Flatten loop emission to HDF5 file for given number of wavelengths
         """
         for wavelength in loop.resolved_wavelengths:
-            emiss,ion_name = loop.get_emission(wavelength.value,return_ion_name=True)
+            emiss,ion_name = loop.get_emission(wavelength,return_ion_name=True)
             dset = hf['{}'.format(str(wavelength.value))]
             dset.attrs['ion_name'] = ion_name
             self.interpolate_and_store(emiss,loop,interp_s,dset,start_index)
