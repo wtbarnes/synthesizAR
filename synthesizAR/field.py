fix units on cartesian velocity when reading from memory

synthesizAR/field.py
@@ -332,7 +332,7 @@ Magnetogram Info:
             s_hat = grad_xyz/np.expand_dims(np.linalg.norm(grad_xyz, axis=1), axis=-1)
             velocity_xyz = np.stack([velocity.value*s_hat[:, 0],
                                      velocity.value*s_hat[:, 1],
-                                     velocity.value*s_hat[:, 2]], axis=2)
+                                     velocity.value*s_hat[:, 2]], axis=2)*velocity.unit
             if savefile is not None:
                 loop.parameters_savefile = savefile
                 with h5py.File(savefile, 'a') as hf:
@@ -351,8 +351,8 @@ Magnetogram Info:
                     dset_velocity.attrs['note'] = 'Velocity in the field-aligned direction'
                     # Cartesian xyz velocity
                     dset_velocity_xyz = hf[loop.name].create_dataset('velocity_xyz',
-                                                                     data=velocity_xyz)
-                    dset_velocity_xyz.attrs['units'] = velocity.unit.to_string()
+                                                                     data=velocity_xyz.value)
+                    dset_velocity_xyz.attrs['units'] = velocity_xyz.unit.to_string()
                     dset_velocity_xyz.attrs['note'] = '''Velocity in the Cartesian coordinate
                                                         system of the extrapolated magnetic
                                                         field. Index 0->x, index 1->y, index