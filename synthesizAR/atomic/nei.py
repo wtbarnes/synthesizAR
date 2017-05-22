7c047ef5d9bd77dd5a0e359dd6fdb1336dcd09a4
Will Barnes <wtb2@rice.edu>
2017-05-22 16:50:11 -0500

    more pep8

--- synthesizAR/atomic/nei.py
+++ synthesizAR/atomic/nei.py
@@ -8,17 +8,18 @@ import logging
 import numpy as np
 import numba
 from ChiantiPy.core import ion
-from ChiantiPy.tools.util import el2z,zion2name
+from ChiantiPy.tools.util import el2z, zion2name
 
-from synthesizAR.util import _numba_interpolator_wrapper,_numba_lagrange_interpolator
+from synthesizAR.util import _numba_interpolator_wrapper, _numba_lagrange_interpolator
 
-def get_ion_data(element,zrange=None,logTa=4.0,logTb=9.0,dlogT=0.01):
+
+def get_ion_data(element, zrange=None, logTa=4.0, logTb=9.0, dlogT=0.01):
     """
     Get needed ionization rates, recombination rates, and equilibrium populations
     for a range of ions for a given element over a course temperature range.
     """
 
-    temperature = 10.**(np.arange(logTa,logTb+dlogT,dlogT))
+    temperature = 10.**(np.arange(logTa, logTb+dlogT, dlogT))
     z = el2z(element)
     if zrange is None:
         zrange = [1,z+1]
@@ -46,11 +47,12 @@ def get_ion_data(element,zrange=None,logTa=4.0,logTb=9.0,dlogT=0.01):
 
     return ionization_rates,recombination_rates,equilibrium_ionization_fractions,temperature
 
+
 @numba.jit(nopython=True)
 def solve_nei_populations(time,temperature,density,ionization_rate,
-                            recombination_rate,eqm_populations,temperature_data,
-                            cutoff=1e-10,eps_r=0.6,eps_d=0.1,safety=0.1,safety_increase=2.,
-                            safety_decrease=10.):
+                          recombination_rate,eqm_populations,temperature_data,
+                          cutoff=1e-10,eps_r=0.6,eps_d=0.1,safety=0.1,safety_increase=2.,
+                          safety_decrease=10.):
     """
     Solve the ionization balance equations for a set of temperature and density timeseries using
     ionization and recombination rates from CHIANTI. The populations are initialized with the 
@@ -73,27 +75,27 @@ def solve_nei_populations(time,temperature,density,ionization_rate,
     dt_old = time[1] - time[0]
     step = 1
     save_this_step = False
-    #evolve in time
+    # evolve in time
     while cur_time < time[-1]:
-        #calculate derivative
+        # calculate derivative
         dydt = cur_density*(cur_ionization_rate[:-2]*cur_populations[:-2]
                             + cur_recombination_rate[1:-1]*cur_populations[2:]
                             - cur_ionization_rate[1:-1]*cur_populations[1:-1]
                             - cur_recombination_rate[:-2]*cur_populations[1:-1])
-        #calculate timestep
+        # calculate timestep
         dt_1 = safety*eps_d/np.fabs(dydt)
         dt_2 = 0.5*(10.**eps_r - 1
                     + np.fabs(10.**(-eps_r) - 1.))*cur_populations[1:-1]/np.fabs(dydt)
         dt_2 = np.where(cur_populations[1:-1]<cutoff,np.inf*np.ones(len(dt_2)),dt_2)
         dt = np.min(np.stack((dt_1,dt_2)))
-        #control timestep increase and decrease
+        # control timestep increase and decrease
         dt = max(dt,dt_old/safety_decrease)
         dt = min(dt,safety_increase*dt_old)
         dt_old = dt
         if cur_time + dt >= time[step]:
             dt = np.fabs(time[step] - cur_time)
             save_this_step = True
-        #update
+        # update
         cur_time += dt
         cur_populations[1:-1] = cur_populations[1:-1] + dydt*dt
         cur_populations /= np.sum(cur_populations)
@@ -105,7 +107,7 @@ def solve_nei_populations(time,temperature,density,ionization_rate,
                                                             cur_temperature)
         cur_recombination_rate = _numba_interpolator_wrapper(temperature_data,recombination_rate,
                                                                 cur_temperature)
-        #save
+        # save
         if save_this_step:
             nei_populations[step,:] = cur_populations
             step += 1
