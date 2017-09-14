831261d5883361dcdc1385d0e5030858074e4487
Will Barnes <wtb2@rice.edu>
2017-09-14 14:47:21 -0500

    fix imports after moving cube structure definitions

--- synthesizAR/util/__init__.py
+++ synthesizAR/util/__init__.py
@@ -3,5 +3,4 @@ Basic utilities needed by all parts of the package
 """
 
 from .util import *
-from .cube import EISCube, EMCube
-from .xml_io import InputHandler,OutputHandler
+from .xml_io import InputHandler, OutputHandler
