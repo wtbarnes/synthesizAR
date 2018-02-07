a2999520f816d2dbe4c586180ea13957c7ab3142
Will Barnes <wtb2@rice.edu>
2018-02-07 01:09:21 -0600

    fix pep8 stuff

--- synthesizAR/field.py
+++ synthesizAR/field.py
@@ -24,8 +24,10 @@ class Field(object):
 
     Parameters
     ----------
-    hmi_fits_file : `str`
-        Path to HMI magnetogram FITS file
+    magnetogram : `sunpy.map.Map`
+        Magnetogram map for the active region
+    fieldlines : `list`
+        List of coordinates and field strengths for each loop
 
     Examples
     --------
@@ -41,7 +43,7 @@ class Field(object):
     def __repr__(self):
         num_loops = len(self.loops) if hasattr(self, 'loops') else 0
         sim_type = self.simulation_type if hasattr(self, 'simulation_type') else ''
-        return f'''synthesizAR Field Object
+        return f'''synthesizAR Active Region Object
 ------------------------
 Number of loops: {num_loops}
 Simulation Type: {sim_type}
@@ -54,15 +56,14 @@ Magnetogram Info:
         Save the components of the field object to be reloaded later.
         """
         if savedir is None:
-            savedir = 'synthesizAR-{}-save_{}'.format(type(self).__name__,
-                                                      (datetime.datetime.now()
-                                                      .strftime('%Y%m%d-%H%M%S')))
+            dt = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+            savedir = f'synthesizAR-{type(self).__name__}-save_{dt}'
         if not os.path.exists(savedir):
             os.makedirs(savedir)
         if not os.path.exists(os.path.join(savedir, 'loops')):
             os.makedirs(os.path.join(savedir, 'loops'))
         for l in self.loops:
-            with open(os.path.join(savedir, 'loops', l.name+'.pickle'), 'wb') as f:
+            with open(os.path.join(savedir, 'loops', f'{l.name}.pickle'), 'wb') as f:
                 pickle.dump(l, f)
         if not os.path.isfile(os.path.join(savedir, 'magnetogram.fits')):
             self.magnetogram.save(os.path.join(savedir, 'magnetogram.fits'))
@@ -117,54 +118,44 @@ Magnetogram Info:
                 interface.configure_input(loop)
                 progress.update()
 
-    def load_loop_simulations(self, interface, savefile=None, **kwargs):
+    def load_loop_simulations(self, interface, savefile, **kwargs):
         """
         Load in loop parameters from hydrodynamic results.
         """
         with ProgressBar(len(self.loops), ipython_widget=kwargs.get('notebook', True)) as progress:
             for loop in self.loops:
-                self.logger.debug('Loading parameters for {}'.format(loop.name))
-                time, electron_temperature, ion_temperature, density, velocity = interface.load_results(loop, **kwargs)
+                (time, electron_temperature, ion_temperature,
+                 density, velocity) = interface.load_results(loop, **kwargs)
                 loop.time = time
-                # convert velocity to cartesian coordinates
+                # convert velocity to loop coordinate system
                 grad_xyz = np.gradient(loop.coordinates.value, axis=0)
-                s_hat = grad_xyz/np.expand_dims(np.linalg.norm(grad_xyz, axis=1), axis=-1)
+                s_hat = grad_xyz / np.expand_dims(np.linalg.norm(grad_xyz, axis=1), axis=-1)
                 velocity_xyz = np.stack([velocity.value*s_hat[:, 0],
                                         velocity.value*s_hat[:, 1],
                                         velocity.value*s_hat[:, 2]], axis=2)*velocity.unit
-                if savefile is not None:
-                    loop.parameters_savefile = savefile
-                    with h5py.File(savefile, 'a') as hf:
-                        if loop.name not in hf:
-                            hf.create_group(loop.name)
-                        # electron temperature
-                        dset_electron_temperature = hf[loop.name].create_dataset('electron_temperature',
-                                                                                 data=electron_temperature.value)
-                        dset_electron_temperature.attrs['units'] = electron_temperature.unit.to_string()
-                        # ion temperature
-                        dset_ion_temperature = hf[loop.name].create_dataset('ion_temperature',
-                                                                            data=ion_temperature.value)
-                        dset_ion_temperature.attrs['units'] = ion_temperature.unit.to_string()
-                        # number density
-                        dset_density = hf[loop.name].create_dataset('density', data=density.value)
-                        dset_density.attrs['units'] = density.unit.to_string()
-                        # field-aligned velocity
-                        dset_velocity = hf[loop.name].create_dataset('velocity', data=velocity.value)
-                        dset_velocity.attrs['units'] = velocity.unit.to_string()
-                        dset_velocity.attrs['note'] = 'Velocity in the field-aligned direction'
-                        # Cartesian xyz velocity
-                        dset_velocity_xyz = hf[loop.name].create_dataset('velocity_xyz',
-                                                                         data=velocity_xyz.value)
-                        dset_velocity_xyz.attrs['units'] = velocity_xyz.unit.to_string()
-                        dset_velocity_xyz.attrs['note'] = '''Velocity in the Cartesian coordinate
-                                                            system of the extrapolated magnetic
-                                                            field. Index 0->x, index 1->y, index
-                                                            2->z.'''
-                else:
-                    loop._electron_temperature = temperature
-                    loop._ion_temperature = temperature
-                    loop._density = density
-                    loop._velocity = velocity
-                    loop._velocity_xyz = velocity_xyz
+                loop.parameters_savefile = savefile
+                with h5py.File(savefile, 'a') as hf:
+                    if loop.name not in hf:
+                        hf.create_group(loop.name)
+                    # electron temperature
+                    dset_electron_temperature = hf[loop.name].create_dataset(
+                                            'electron_temperature', data=electron_temperature.value)
+                    dset_electron_temperature.attrs['units'] = electron_temperature.unit.to_string()
+                    # ion temperature
+                    dset_ion_temperature = hf[loop.name].create_dataset('ion_temperature',
+                                                                        data=ion_temperature.value)
+                    dset_ion_temperature.attrs['units'] = ion_temperature.unit.to_string()
+                    # number density
+                    dset_density = hf[loop.name].create_dataset('density', data=density.value)
+                    dset_density.attrs['units'] = density.unit.to_string()
+                    # field-aligned velocity
+                    dset_velocity = hf[loop.name].create_dataset('velocity', data=velocity.value)
+                    dset_velocity.attrs['units'] = velocity.unit.to_string()
+                    dset_velocity.attrs['note'] = 'Velocity in the field-aligned direction'
+                    # Cartesian xyz velocity
+                    dset_velocity_xyz = hf[loop.name].create_dataset('velocity_xyz',
+                                                                     data=velocity_xyz.value)
+                    dset_velocity_xyz.attrs['units'] = velocity_xyz.unit.to_string()
+                    dset_velocity_xyz.attrs['note'] = 'velocity in HEEQ system'
 
                 progress.update()
--- synthesizAR/loop.py
+++ synthesizAR/loop.py
@@ -16,8 +16,10 @@ class Loop(object):
 
     Parameters
     ----------
-    Attributes
-    ----------
+    name : `str`
+    coordinates : `astropy.Quantity`
+    field_strength : `astropy.Quantity`
+
     Notes
     -----
     """
@@ -29,17 +31,12 @@ class Loop(object):
         self.field_strength = field_strength.to(u.gauss)
 
     def __repr__(self):
-        return '''
-Name : {name}
-Loop full-length, 2L : {loop_length:.3f}
-Footpoints : ({fp0}),({fp1}) {fpu}
-Maximum field strength : {max_b:.2f}
-        '''.format(name=self.name,
-                   loop_length=self.full_length.to(u.Mm),
-                   fp0=','.join(['{:.3g}'.format(l) for l in self.coordinates[0,:].value]),
-                   fp1=','.join(['{:.3g}'.format(l) for l in self.coordinates[-1,:].value]),
-                   fpu=self.coordinates.unit.to_string(),
-                   max_b=np.max(self.field_strength))
+        fp0 = ','.join(['{:.3g}'.format(l) for l in self.coordinates[0, :].value])
+        fp1 = ','.join(['{:.3g}'.format(l) for l in self.coordinates[-1, :].value])
+        return f'''Name : {self.name}
+Loop full-length, 2L : {self.full_length.to(u.Mm):.3f}
+Footpoints : ({fp0}),({fp1}) {self.coordinates.unit.to_string()}
+Maximum field strength : {np.max(self.field_strength):.2f}'''
 
     @property
     def field_aligned_coordinate(self):
@@ -47,7 +44,7 @@ Maximum field strength : {max_b:.2f}
         Field-aligned coordinate :math:`s`. This will have the same units the original coordinates.
         """
         return np.append(0., np.linalg.norm(np.diff(self.coordinates.value, axis=0),
-                                            axis=1).cumsum())*self.coordinates.unit
+                                            axis=1).cumsum()) * self.coordinates.unit
 
     @property
     def full_length(self):
@@ -55,7 +52,7 @@ Maximum field strength : {max_b:.2f}
         Loop full-length :math:`2L`. This will have the same units as the original coordinates.
         """
         return np.sum(np.linalg.norm(np.diff(self.coordinates.value, axis=0),
-                                     axis=1))*self.coordinates.unit
+                                     axis=1)) * self.coordinates.unit
 
     @property
     def electron_temperature(self):
@@ -65,7 +62,7 @@ Maximum field strength : {max_b:.2f}
         """
         with h5py.File(self.parameters_savefile, 'r') as hf:
             dset = hf['/'.join([self.name, 'electron_temperature'])]
-            temperature = np.array(dset)*u.Unit(dset.attrs['units'])
+            temperature = np.array(dset) * u.Unit(dset.attrs['units'])
         return temperature
 
     @property
@@ -76,7 +73,7 @@ Maximum field strength : {max_b:.2f}
         """
         with h5py.File(self.parameters_savefile, 'r') as hf:
             dset = hf['/'.join([self.name, 'ion_temperature'])]
-            temperature = np.array(dset)*u.Unit(dset.attrs['units'])
+            temperature = np.array(dset) * u.Unit(dset.attrs['units'])
         return temperature
 
     @property
@@ -87,7 +84,7 @@ Maximum field strength : {max_b:.2f}
         """
         with h5py.File(self.parameters_savefile, 'r') as hf:
             dset = hf['/'.join([self.name, 'density'])]
-            density = np.array(dset)*u.Unit(dset.attrs['units'])
+            density = np.array(dset) * u.Unit(dset.attrs['units'])
         return density
 
     @property
@@ -98,7 +95,7 @@ Maximum field strength : {max_b:.2f}
         """
         with h5py.File(self.parameters_savefile, 'r') as hf:
             dset = hf['/'.join([self.name, 'velocity'])]
-            velocity = np.array(dset)*u.Unit(dset.attrs['units'])
+            velocity = np.array(dset) * u.Unit(dset.attrs['units'])
         return velocity
 
     @property
@@ -109,5 +106,5 @@ Maximum field strength : {max_b:.2f}
         """
         with h5py.File(self.parameters_savefile, 'r') as hf:
             dset = hf['/'.join([self.name, 'velocity_xyz'])]
-            velocity_xyz = np.array(dset)*u.Unit(dset.attrs['units'])
+            velocity_xyz = np.array(dset) * u.Unit(dset.attrs['units'])
         return velocity_xyz
