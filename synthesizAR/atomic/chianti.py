a1d8e43b77e9901ed27d3ed83eaabc55437f7972
Will Barnes <wtb2@rice.edu>
2018-04-04 01:56:39 -0500

    make __getitem__ return the Ion subclass

--- synthesizAR/atomic/chianti.py
+++ synthesizAR/atomic/chianti.py
@@ -21,6 +21,11 @@ __all__ = ['Element', 'Ion', 'list_elements']
 
 
 class Element(fiasco.Element):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # FIXME: this can be removed once fiasco includes emissivity calculation
+        self._ion_list = [Ion(i.ion_name, i.temperature) for i in self._ion_list]
         
     @u.quantity_input
     def non_equilibrium_ionization(self, time: u.s, temperature: u.K, density: u.cm**(-3),
--- synthesizAR/atomic/emission_models.py
+++ synthesizAR/atomic/emission_models.py
@@ -21,6 +21,8 @@ class EmissionModel(fiasco.IonCollection):
     @u.quantity_input
     def __init__(self, density: u.cm**(-3), *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # FIXME: this line can be removed once Ion subclass is no longer needed
+        self._ion_list = [Ion(i.ion_name, i.temperature) for i in self._ion_list]
         self.temperature = self[0].temperature
         self.density = density
         self.resolved_wavelengths = kwargs.get('resolved_wavelengths', {})
