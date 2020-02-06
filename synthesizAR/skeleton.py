e858e772523a7b26c80303e55a0ab8eec9c1f699
Will Barnes <will.t.barnes@gmail.com>
2020-02-06 18:42:17 -0500

    restore,save are now to_asdf,from_asdf and serialize to asdf rather than hdf5

--- synthesizAR/skeleton.py
+++ synthesizAR/skeleton.py
@@ -1,19 +1,15 @@
 """
 Container for fieldlines in three-dimensional magnetic skeleton
 """
-import os
-import datetime
-
-import numpy as np
-from sunpy.coordinates import HeliographicStonyhurst
+from scipy.interpolate import splev, splrep
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astropy.utils.console import ProgressBar
+import asdf
 import h5py
 
 from synthesizAR import Loop
 from synthesizAR.extrapolate import peek_fieldlines
-from synthesizAR.util import get_keys
 
 
 class Skeleton(object):
@@ -54,73 +50,47 @@ class Skeleton(object):
         return cls(loops)
 
     def __repr__(self):
-        sim_type = self.simulation_type if hasattr(self, 'simulation_type') else ''
         return f'''synthesizAR Skeleton Object
 ------------------------
-Number of loops: {len(self.loops)}
-Simulation Type: {sim_type}'''
+Number of loops: {len(self.loops)}'''
 
-    def save(self, savedir=None):
+    def to_asdf(self, filename):
         """
-        Save the components of the field object to be reloaded later.
+        Serialize this instance of `Skeleton` to an ASDF file
         """
-        if savedir is None:
-            dt = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-            savedir = f'synthesizAR-{type(self).__name__}-save_{dt}'
-        if not os.path.exists(savedir):
-            os.makedirs(savedir)
-        with h5py.File(os.path.join(savedir, 'loops.h5'), 'w') as hf:
-            for i, loop in enumerate(self.loops):
-                grp = hf.create_group(loop.name)
-                grp.attrs['index'] = i
-                if hasattr(loop, 'parameters_savefile'):
-                    grp.attrs['parameters_savefile'] = loop.parameters_savefile
-                else:
-                    grp.attrs['parameters_savefile'] = ''
-                ds = grp.create_dataset('coordinates', data=loop.coordinates.cartesian.xyz.value)
-                ds.attrs['unit'] = loop.coordinates.cartesian.xyz.unit.to_string()
-                ds = grp.create_dataset('field_strength', data=loop.field_strength.value)
-                ds.attrs['unit'] = loop.field_strength.unit.to_string()
+        tree = {}
+        for l in self.loops:
+            tree[l.name] = {
+                'field_strength': l.field_strength,
+                'coordinates': l.coordinates,
+                'model_results_filename': l.model_results_filename,
+            }
+        with asdf.AsdfFile(tree) as asdf_file:
+            asdf_file.write_to(filename)
 
     @classmethod
-    def restore(cls, savedir):
+    def from_asdf(cls, filename):
         """
-        Restore the field from a set of serialized files
-
-        Parameters
-        ----------
-        savedir: `str`
-            Path to directory that contains savefiles
+        Restore a `Skeleton` instance from an ASDF file
 
         Examples
         --------
         >>> import synthesizAR
-        >>> restored_field = synthesizAR.Skeleton.restore('/path/to/restored/field/dir') # doctest: +SKIP
+        >>> restored_field = synthesizAR.Skeleton.from_asdf('/path/to/skeleton.asdf') # doctest: +SKIP
         """
+        exclude_keys = ['asdf_library', 'history']
         loops = []
-        indices = []
-        with h5py.File(os.path.join(savedir, 'loops.h5'), 'r') as hf:
-            for grp_name in hf:
-                grp = hf[grp_name]
-                x = u.Quantity(grp['coordinates'][0, :],
-                               get_keys(grp['coordinates'].attrs, ('unit', 'units')))
-                y = u.Quantity(grp['coordinates'][1, :],
-                               get_keys(grp['coordinates'].attrs, ('unit', 'units')))
-                z = u.Quantity(grp['coordinates'][2, :],
-                               get_keys(grp['coordinates'].attrs, ('unit', 'units')))
-                coordinates = SkyCoord(
-                    x=x, y=y, z=z, frame=HeliographicStonyhurst, representation='cartesian')
-                field_strength = u.Quantity(
-                    grp['field_strength'],
-                    get_keys(grp['field_strength'].attrs, ('unit', 'units')))
-                l = Loop(grp_name, coordinates=coordinates, field_strength=field_strength)
-                if grp.attrs['parameters_savefile']:
-                    l.parameters_savefile = grp.attrs['parameters_savefile']
-                loops.append(l)
-                indices.append(grp.attrs['index'])
-        # NOTE: this to make sure the loops are in the same order as before
-        loops, _ = zip(*sorted(zip(loops, indices), key=lambda x: x[1]))
-
+        with asdf.open(filename) as af:
+            for k in af.keys():
+                if k in exclude_keys:
+                    continue
+                model_results_filename = af.tree[k].get('model_results_filename', None)
+                loops.append(Loop(
+                    k,
+                    SkyCoord(af.tree[k]['coordinates']),
+                    af.tree[k]['field_strength'],
+                    model_results_filename=model_results_filename,
+                ))
         return cls(loops)
 
     def peek(self, magnetogram, **kwargs):
@@ -134,42 +104,46 @@ Simulation Type: {sim_type}'''
         """
         Configure hydrodynamic simulations for each loop object
         """
-        self.simulation_type = interface.name
         with ProgressBar(len(self.loops), ipython_widget=kwargs.get('notebook', True)) as progress:
             for loop in self.loops:
                 interface.configure_input(loop)
                 progress.update()
 
-    def load_loop_simulations(self, interface, savefile, **kwargs):
+    def load_loop_simulations(self, interface, filename, **kwargs):
         """
         Load in loop parameters from hydrodynamic results.
         """
-        with h5py.File(savefile, 'w') as hf:
+        with h5py.File(filename, 'w') as hf:
             with ProgressBar(len(self.loops),
                              ipython_widget=kwargs.get('notebook', True),) as progress:
                 for loop in self.loops:
                     # Load in parameters from interface
                     (time, electron_temperature, ion_temperature,
                      density, velocity) = interface.load_results(loop)
-                    # convert velocity to loop coordinate system
-                    grad_xyz = np.gradient(loop.coordinates.cartesian.xyz.value, axis=1)
-                    s_hat = grad_xyz / np.linalg.norm(grad_xyz, axis=0)
-                    velocity_x = velocity * s_hat[0, :]
-                    velocity_y = velocity * s_hat[1, :]
-                    velocity_z = velocity * s_hat[2, :]
+                    # Convert velocity to loop coordinate system
+                    # NOTE: the direction is evaluated at the left edges + the last right edge.
+                    # But the velocity is evaluated at the center of each cell so we need
+                    # to interpolate the direction to the cell centers for each component
+                    s = loop.field_aligned_coordinate.to(u.Mm).value
+                    s_center = loop.field_aligned_coordinate_center.to(u.Mm).value
+                    s_hat = loop.coordinate_direction.value
+                    velocity_x = velocity * splev(s_center, splrep(s, s_hat[0, :]))
+                    velocity_y = velocity * splev(s_center, splrep(s, s_hat[1, :]))
+                    velocity_z = velocity * splev(s_center, splrep(s, s_hat[2, :]))
                     # Write to file
-                    loop.parameters_savefile = savefile
+                    loop.model_results_filename = filename
                     grp = hf.create_group(loop.name)
+                    grp.attrs['simulation_type'] = interface.name
                     # time
                     dset_time = grp.create_dataset('time', data=time.value)
                     dset_time.attrs['unit'] = time.unit.to_string()
                     # electron temperature
-                    dset_electron_temperature = grp.create_dataset('electron_temperature',
-                                                                   data=electron_temperature.value)
+                    dset_electron_temperature = grp.create_dataset(
+                        'electron_temperature', data=electron_temperature.value)
                     dset_electron_temperature.attrs['unit'] = electron_temperature.unit.to_string()
                     # ion temperature
-                    dset_ion_temperature = grp.create_dataset('ion_temperature',
-                                                              data=ion_temperature.value)
+                    dset_ion_temperature = grp.create_dataset(
+                        'ion_temperature', data=ion_temperature.value)
                     dset_ion_temperature.attrs['unit'] = ion_temperature.unit.to_string()
                     # number density
                     dset_density = grp.create_dataset('density', data=density.value)
@@ -188,5 +162,4 @@ Simulation Type: {sim_type}'''
                     dset_velocity_z = grp.create_dataset('velocity_z', data=velocity_z.value)
                     dset_velocity_z.attrs['unit'] = velocity_z.unit.to_string()
                     dset_velocity_z.attrs['note'] = 'z-component of velocity in HEEQ coordinates'
-
                     progress.update()
