fix up scaling law interfaces

synthesizAR/interfaces/martens.py
@@ -19,6 +19,8 @@ class MartensInterface(object):
 
     Parameters
     ----------
+    heating_constant : `~astropy.units.Quantity`
+        Constant of proportionality for heating rate scaling.
     temperature_cutoff : `~astropy.units.Quantity`, optional
         Lowest possible temperature in the loop. The Martens scaling laws
         permit temperatures of 0 K at the base of the loop which are unphysical.
@@ -40,7 +42,8 @@ class MartensInterface(object):
     def load_results(self, loop):
         time = u.Quantity([0, ], 's')
         s_half = np.linspace(0, 1, 1000)*loop.length/2
-        msl = MartensScalingLaws(s_half, self.heating_constant, **self.model_parameters)
+        H = self.get_heating_constant(loop)
+        msl = MartensScalingLaws(s_half, H, **self.model_parameters)
         # Make sure there are no temperatures below specified cutoff
         msl_temperature = np.where(msl.temperature < self.temperature_cutoff,
                                    self.temperature_cutoff,
@@ -61,3 +64,10 @@ class MartensInterface(object):
         velocity = np.ones(time.shape+s_center.shape) * np.nan * u.cm/u.s
 
         return time, temperature, temperature, density, velocity
+
+    def get_heating_constant(self, loop):
+        """
+        Override this to get a more complicated relationship between the loop
+        and the heating rate
+        """
+        return self.heating_constant

synthesizAR/interfaces/rtv.py
@@ -27,14 +27,13 @@ class RTVInterface(object):
     """
     name = 'RTV'
 
-    def __init__(self, plasma_beta=0.001, rtv_kwargs=None):
-        self.plasma_beta = plasma_beta
+    def __init__(self, heating_model, rtv_kwargs=None):
+        self.heating_model = heating_model
         self.rtv_kwargs = {} if rtv_kwargs is None else rtv_kwargs
 
     def load_results(self, loop):
-        pressure = loop.field_strength.mean().to(u.G).value**2 / 8 / np.pi * self.plasma_beta
-        pressure = pressure * u.dyne / (u.cm**2)
-        rtv = RTVScalingLaws(loop.length/2, pressure=pressure, **self.rtv_kwargs)
+        heating_rate = self.heating_model.get_heating_rate(loop)
+        rtv = RTVScalingLaws(loop.length/2, heating_rate=heating_rate, **self.rtv_kwargs)
         time = u.Quantity([0, ], 's')
         shape = time.shape+loop.field_aligned_coordinate_center.shape
         temperature = np.ones(shape) * rtv.max_temperature