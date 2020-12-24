03a7e3bd23f308019de61f59dca59d015a2d4fdc
Will Barnes <will.t.barnes@gmail.com>
2020-12-24 13:20:09 -0500

    refactor hydrad interface

--- synthesizAR/interfaces/hydrad/__init__.py
+++ synthesizAR/interfaces/hydrad/__init__.py
@@ -2,4 +2,3 @@
 Interface to the HYDRAD field-aligned hydrodynamic model
 """
 from .hydrad import *
-from .helpers import *
--- synthesizAR/interfaces/hydrad/hydrad.py
+++ synthesizAR/interfaces/hydrad/hydrad.py
@@ -6,10 +6,12 @@ import os
 import numpy as np
 from scipy.interpolate import splrep, splev
 import astropy.units as u
+import astropy.constants as const
+import sunpy.sun.constants as sun_const
+
 from pydrad.configure import Configure
 from pydrad.parse import Strand
 
-from .helpers import cross_section_coefficients, gravity_coefficients
 
 __all__ = ['HYDRADInterface']
 
@@ -19,10 +21,7 @@ class HYDRADInterface(object):
     Interface to the HYDrodynamics and RADiation (HYDRAD) code
 
     Configure, interpolate, and load results for HYDRAD simulations
-    for each loop in the magnetic skeleton. Note that the load function
-    will also reinterpolate the loop coordinates and magnetic field
-    strength for each loop such that the spatial resolution is
-    `delta_s_uniform`.
+    for each loop in the magnetic skeleton.
 
     Parameters
     ----------
@@ -36,22 +35,55 @@ class HYDRADInterface(object):
     heating_model: object
         Instance of a heating model class that describes when and
         where the heating should occur along the loop
+    use_gravity: `bool`, optional
+        If true, use the loop coordinates to determine the gravitational stratification
+    use_magnetic_field: `bool`, optional
+        If true, use the magnetic field profile to determine the loop expansion factor
+    use_initial_conditions: `bool`, optional
+        If true, use only the hydrostatic initial conditions as the model
+    maximum_chromosphere_ratio: `float`, optional
+        Maximum allowed ratio between the loop length and the total chromsphere depth.
+        If specified, `general.footpoint_height` will be set to this ratio times the
+        loop length if ``2 * general.footpoint_height / length`` is greater than this
+        ratio.
     """
     name = 'HYDRAD'
 
     @u.quantity_input
-    def __init__(self, base_config, hydrad_dir, output_dir, heating_model):
+    def __init__(self,
+                 base_config,
+                 hydrad_dir,
+                 output_dir,
+                 heating_model,
+                 use_gravity=True,
+                 use_magnetic_field=True,
+                 use_initial_conditions=False,
+                 maximum_chromosphere_ratio=None):
         self.base_config = base_config
         self.hydrad_dir = hydrad_dir
         self.output_dir = output_dir
         self.heating_model = heating_model
+        self.use_gravity = use_gravity
+        self.use_magnetic_field = use_magnetic_field
+        self.use_initial_conditions = use_initial_conditions
+        self.maximum_chromosphere_ratio = maximum_chromosphere_ratio
 
     def configure_input(self, loop):
         config = self.base_config.copy()
-        config['general']['loop_length'] = loop.length
-        config['initial_conditions']['heating_location'] = loop.length / 2.
-        config['general']['poly_fit_gravity'] = gravity_coefficients(loop)
-        config['general']['poly_fit_magnetic_field'] = cross_section_coefficients(loop)
+        # NOTE: Truncate precision in loop length here as passing a loop length
+        # with too many significant figures to HYDRAD can cause issues with the
+        # initical conditions calculation. Here, we truncate the loop length such
+        # that it has 1 significant figure when expressed in Mm
+        length = float(f'{loop.length.to("Mm").value:.1f}') * u.Mm
+        config['general']['loop_length'] = length
+        config['initial_conditions']['heating_location'] = length / 2.
+        if self.maximum_chromosphere_ratio:
+            config['general']['footpoint_height'] = min(config['general']['footpoint_height'],
+                                                        self.maximum_chromosphere_ratio * length / 2)
+        if self.use_gravity:
+            config['general']['poly_fit_gravity'] = self.configure_gravity_fit(loop)
+        if self.use_magnetic_field:
+            config['general']['poly_fit_magnetic_field'] = self.configure_magnetic_field_fit(loop)
         config = self.heating_model.calculate_event_properties(config, loop)
         c = Configure(config)
         c.setup_simulation(os.path.join(self.output_dir, loop.name),
@@ -61,7 +93,12 @@ class HYDRADInterface(object):
     def load_results(self, loop):
         loop_coord_center = loop.field_aligned_coordinate_center.to(u.cm).value
         s = Strand(os.path.join(self.output_dir, loop.name))
-        shape = s.time.shape + loop_coord_center.shape
+        if self.use_initial_conditions:
+            time = s.initial_conditions.time.reshape((1,))
+            s = [s.initial_conditions]
+        else:
+            time = s.time
+        shape = time.shape + loop_coord_center.shape
         electron_temperature = np.zeros(shape)
         ion_temperature = np.zeros(shape)
         density = np.zeros(shape)
@@ -78,9 +115,33 @@ class HYDRADInterface(object):
             velocity[i, :] = splev(loop_coord_center, tsk, ext=0)
 
         return (
-            s.time,
+            time,
             electron_temperature*u.K,
             ion_temperature*u.K,
             density*u.cm**(-3),
             velocity*u.cm/u.s,
         )
+
+    def configure_gravity_fit(self, loop):
+        r_hat = u.Quantity(np.stack([
+            np.sin(loop.coordinate.spherical.lat)*np.cos(loop.coordinate.spherical.lon),
+            np.sin(loop.coordinate.spherical.lat)*np.sin(loop.coordinate.spherical.lon),
+            np.cos(loop.coordinate.spherical.lat)
+        ]))
+        r_hat_dot_s_hat = (r_hat * loop.coordinate_direction).sum(axis=0)
+        g_parallel = -sun_const.surface_gravity * (
+            (const.R_sun / loop.coordinate.spherical.distance)**2) * r_hat_dot_s_hat
+        return {
+            'order': self.base_config['general']['poly_fit_gravity']['order'],
+            'domains': self.base_config['general']['poly_fit_gravity']['domains'],
+            'x': loop.field_aligned_coordinate_norm.decompose().value,
+            'y': g_parallel,
+        }
+
+    def configure_magnetic_field_fit(self, loop):
+        return {
+            'order': self.base_config['general']['poly_fit_magnetic_field']['order'],
+            'domains': self.base_config['general']['poly_fit_magnetic_field']['domains'],
+            'x': loop.field_aligned_coordinate_norm.decompose().value,
+            'y': loop.field_strength,
+        }
