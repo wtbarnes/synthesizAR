a4eed68d85ac866b5f8ee4cb00e1231ef7300936
Will Barnes <will.t.barnes@gmail.com>
2021-01-25 23:31:43 -0500

    simplify semi-circular loop; add function for arcade geometries

--- synthesizAR/models/geometry.py
+++ synthesizAR/models/geometry.py
@@ -7,7 +7,7 @@ from astropy.coordinates import SkyCoord
 import astropy.constants as const
 import sunpy.coordinates
 
-__all__ = ['semi_circular_loop', 'semi_circular_bundle']
+__all__ = ['semi_circular_loop', 'semi_circular_bundle', 'semi_circular_arcade']
 
 
 @u.quantity_input
@@ -36,21 +36,23 @@ def semi_circular_loop(length: u.cm=None,
     n_points : `int`, optional
         Number of points in the coordinate. Only used if `s` is not specified.
     offset : `~astropy.units.Quantity`
-        Offset in the direction perpendicular to the arcade
+        Offset in the direction perpendicular to loop, convenient for simulating
+        arcade geometries.
     gamma : `~astropy.units.Quantity`
-        Orientation of the arcade relative to the HCC y-axis
+        Orientation of the arcade relative to the HCC y-axis. `gamma=0` corresponds
+        to a loop who's axis is oriented parallel to the HCC y-axis.
     """
     if s is None and length is None:
         raise ValueError('Must specify field-aligned coordinate or loop length')
     if s is None:
-        s = np.linspace(0, length, n_points)
+        angles = np.linspace(0, 1, n_points) * np.pi * u.rad
     elif length is None:
         length = s[-1]
+        angles = s / length
     else:
         raise ValueError('Specify either length or field-aligned coordinate but not both.')
-    z = length / np.pi * np.sin(np.pi * u.rad * s/length)
-    x = np.sqrt((length / np.pi)**2 - z**2)
-    x = np.where(s < length/2, -x, x)
+    z = length / np.pi * np.sin(angles)
+    x = length / np.pi * np.cos(angles)
     # Define origin in HCC coordinates such that the midpoint of the loop
     # is centered on the origin at the solar surface
     if observer is None:
@@ -61,7 +63,6 @@ def semi_circular_loop(length: u.cm=None,
         observer=observer,
         obstime=observer.obstime if obstime is None else obstime,
     )
-    # Offset along the y-axis, convenient for creating loop arcades
     return SkyCoord(x=-offset * np.sin(gamma) + x * np.cos(gamma),
                     y=offset * np.cos(gamma) + x * np.sin(gamma),
                     z=z + const.R_sun,
@@ -92,3 +93,38 @@ def semi_circular_bundle(length: u.cm, radius: u.cm, num_strands, **kwargs):
     max_offset = np.sqrt(radius**2 - (1/np.pi * (lengths - length))**2)
     offset = np.random.random_sample(size=num_strands)*2*max_offset - max_offset
     return [semi_circular_loop(length=l, offset=o, **kwargs) for l,o in zip(lengths, offset)]
+
+
+@u.quantity_input
+def semi_circular_arcade(length: u.cm, width: u.deg, num_strands, observer, **kwargs):
+    """
+    Generate an arcade of `num_loops` of full length `length` evenly spaced in
+    over an HGS angular width of `width` centered on a location specified by
+    `observer`
+
+    Parameters
+    ----------
+    length : `~astropy.units.Quantity`
+        Full-length of each strand in the arcade
+    width : `~astropy.units.Quantity`
+        Angular width of the arcade. This can either be in the latitudinal or
+        longitudinal direction depending on the orientation of the loops as
+        specified by `gamma`
+    num_strands : `int`
+        Number of strands in the arcade
+    observer : `~astropy.coordinates.SkyCoord`
+        Observer that defines the "center" loop. Offsets equally-spaced between
+        `-width/2` and `width/2` are calculated relative to this observer.
+
+    See Also
+    --------
+    semi_circular_loop
+    """
+    gamma = kwargs.pop('gamma', 90*u.deg)
+    strands = []
+    for o in np.linspace(-width/2, width/2, num_strands):
+        obs = SkyCoord(lon=observer.lon - o*np.sin(gamma),
+                       lat=observer.lat + o*np.cos(gamma),
+                       frame=observer.frame)
+        strands.append(semi_circular_loop(length=length, observer=obs, gamma=gamma, **kwargs))
+    return strands
