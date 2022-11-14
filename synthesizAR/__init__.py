5df3729f343aaa2102c57f5c46973185703da24e
Will Barnes <will.t.barnes@gmail.com>
2022-11-14 15:21:59 -0500

    use DN unit in astropy

--- synthesizAR/__init__.py
+++ synthesizAR/__init__.py
@@ -7,17 +7,6 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-try:
-    # Alias counts as DN for convenience
-    import astropy.units
-    DN = astropy.units.def_unit('DN', astropy.units.count)
-    astropy.units.add_enabled_units(DN)
-except ValueError:
-    # If a unit with the name 'DN' has already been registered,
-    # a ValueError will be thrown, but we don't want our import
-    # to explode over this.
-    pass
-
 
 from .loop import *
 from .skeleton import *
