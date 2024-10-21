de37652cb4148b7fed571d7282f6daa5b5502cf2
Will Barnes <will.t.barnes@gmail.com>
2024-10-21 01:02:45 -0400

    use sunpy is_visible method

--- synthesizAR/util/util.py
+++ synthesizAR/util/util.py
@@ -6,7 +6,6 @@ import astropy.constants as const
 import astropy.units as u
 import numpy as np
 import sunpy.coordinates
-import sunpy.sun.constants as sun_const
 import warnings
 
 from astropy.coordinates import SkyCoord
@@ -20,7 +19,6 @@ __all__ = [
     'los_velocity',
     'coord_in_fov',
     'find_minimum_fov',
-    'is_visible',
     'from_pfsspack',
     'from_pfsspy',
     'change_obstime',
@@ -96,28 +94,6 @@ def find_minimum_fov(coordinates, padding=None):
         frame=coordinates.frame
     )
     return bottom_left_corner, top_right_corner
-
-
-def is_visible(coords, observer):
-    """
-    Create mask of coordinates not blocked by the solar disk.
-
-    Parameters
-    ----------
-    coords : `~astropy.coordinates.SkyCoord`
-        Helioprojective oordinates of the object(s) of interest
-    observer : `~astropy.coordinates.SkyCoord`
-        Heliographic-Stonyhurst Location of the observer
-    """
-    theta_x = coords.Tx
-    theta_y = coords.Ty
-    distance = coords.distance
-    rsun_obs = ((sun_const.radius / (observer.radius - sun_const.radius)).decompose()
-                * u.radian).to(u.arcsec)
-    off_disk = np.sqrt(theta_x**2 + theta_y**2) > rsun_obs
-    in_front_of_disk = distance - observer.radius < 0.
-
-    return np.any(np.stack([off_disk, in_front_of_disk], axis=1), axis=1)
 
 
 def from_pfsspack(pfss_fieldlines):
