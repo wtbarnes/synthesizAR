c22f3709912b76007ae27a404e23c141bad57576
Will Barnes <will.t.barnes@gmail.com>
2025-09-11 16:08:29 -0400

    add function to calculate cargill cooling time

---
+++ synthesizAR/models/cooling.py
@@ -0,0 +1,37 @@
+"""
+Useful models for cooling of coronal loops.
+"""
+import astropy.constants as const
+import astropy.units as u
+import numpy as np
+
+from .scaling_laws import KAPPA_0
+
+__all__ = ['cargill_cooling_time']
+
+
+@u.quantity_input
+def cargill_cooling_time(strand, heating_rate) -> u.s:
+    """
+    Estimate loop cooling time for a given heating rate.
+
+    These estimates of the cooling time are derived primarily by those expressions
+    given in the Appendix of :cite:t:`cargill_active_2014`.
+    """
+    half_length = strand.length/2
+    # set some constants
+    alpha = -0.5
+    chi = 6e-20*u.erg*u.cm**3/u.s/(u.K**alpha)
+    c1,c2,c3 = 2.0,0.9,0.6
+    gamma = 5./3.
+    # estimate max n0T0
+    T0 = c2*(7.*half_length**2*heating_rate/2./KAPPA_0)**(2./7.)
+    top_term = heating_rate - 2.*KAPPA_0*(T0**(3.5))/(7.*(c2**2.5)*c3*(half_length**2)*gamma)
+    bottom_term = c1*chi*(T0**alpha)*(1. - c2/c3/gamma)
+    n0 = np.sqrt(top_term/bottom_term)
+    n0T0 = n0*T0
+    # Cargill cooling expression
+    term1 = (2. - alpha)/(1. - alpha)
+    term2 = (KAPPA_0**(4. - 2.*alpha))*(chi**7)
+    term3 = ((half_length)**(8. - 4.*alpha))/(n0T0**(3+2.*alpha))
+    return term1*3.*const.k_B*(1/term2*term3)**(1/(11. - 2.*alpha))
